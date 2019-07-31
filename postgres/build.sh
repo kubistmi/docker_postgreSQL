f8a4bfb09659c24339bcc980392943493a1d7d83
Michal Kubista <kubistmi@gmail.com>
2019-08-01 00:24:11 +0200

    Finalising build and run scripts.

--- postgres/build.sh
+++ postgres/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t $D_IMAGE --build-arg D_USER='' --build-arg D_PASSWD='' --build-arg D_DATABASE='' .
+source config.sh
+docker build -t $D_IMAGE --build-arg D_USER=$D_USER --build-arg D_PASSWD=$D_PASSWD --build-arg D_DATABASE=$D_DATABASE .
--- postgres/run.sh
+++ postgres/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source config.sh
 docker run --restart always --name $D_CONTAINER -d -p 5432:5432 -v $D_LOCAL:$D_REMOTE:Z $D_IMAGE
